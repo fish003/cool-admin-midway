a213c8bb32a1f0aff3c246bb9862c8d4951127fd
张帮雄 <zhangbx20@vanke.com>
2024-01-10 00:21:39 +0800

    Update config.local.ts
    test

--- src/config/config.local.ts
+++ src/config/config.local.ts
@@ -12,7 +12,7 @@ export default {
         host: '127.0.0.1',
         port: 3306,
         username: 'usersql',
-        password: 'EtxPhGrFZJPdMKtK',
+        password: 'EtxPhGrFZJPdMKtK1',
         database: 'namesql',
         // 自动建表 注意：线上部署的时候不要使用，有可能导致数据丢失
         synchronize: false,
